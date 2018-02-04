f574776481f1259170e6af211edfd791dcef7dce
CristhianMotoche <cristhian.motoche@gmail.com>
2018-02-04 12:25:01 -0500

    Update static method example

--- EntryLevel/Methods/Main.java
+++ EntryLevel/Methods/Main.java
@@ -7,11 +7,19 @@ class Main {
 }
 
 class MyClass {
+  private static int INT = 3;
+  private int simpleInt = 3;
   static void classMethod(){
+    MyClass.INT = 4;
+    // simpleInt cannot be assigned inside an static context
+    // this.simpleInt = 4;
     System.out.println("Este método no depende de una instancia de la clase");
+    System.out.println("INT ahora es: " + MyClass.INT);
   }
 
   public void instanceMethod(){
+    this.simpleInt = 4;
     System.out.println("Este método depende de una instancia de la clase");
+    System.out.println("simpleInt ahora es: " + this.simpleInt);
   }
 }
